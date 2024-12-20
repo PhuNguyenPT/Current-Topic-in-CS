834bd297b599f3b9b37c60e0b9eb0246a8438b1e
phu <phu.npt01@gmail.com>
2024-12-20 12:27:38 +0700

    update InserDimRecencyScore

--- InsertTables/InsertDimRecencyScore.sql
+++ InsertTables/InsertDimRecencyScore.sql
@@ -26,7 +26,12 @@ GlobalMinMax AS (
     FROM 
         [CompanyX].[Sales].[SalesOrderHeader]
 ),
-
+DateDiffGlobal AS (
+	SELECT
+		DATEDIFF(DAY, GlobalMin, GETDATE()) AS GlobalMin,
+		DATEDIFF(DAY, GlobalMax, GETDATE()) AS GlobalMax
+	FROM GlobalMinMax
+),
 RecencyQuantileData AS (
     -- Step 4: Partition customers into 4 quantiles based on recency (DaysSinceLastOrder)
     SELECT 
@@ -55,36 +60,34 @@ RecencyQuantileLimits AS (
         MAX(DaysSinceLastOrder) AS MaxRecencyValue
     FROM FilteredRecencyQuantiles
 ),
-
 RecencyScoreRanges AS (
     -- Step 7: Calculate the Score ranges for recency based on Quantiles 2 to 4
     SELECT 
-        Score,
-        -- For Score 1, LowerLimit is the GlobalMax (oldest date)
+        s.Score,
+        -- For Score 10, LowerLimit is the smallest DaysSinceLastOrder (most recent)
         CASE 
-            WHEN Score = 1 THEN (SELECT DATEDIFF(DAY, GlobalMax, GETDATE()) FROM GlobalMinMax)
-            ELSE MinRecencyValue + (MaxRecencyValue - MinRecencyValue) / 10.0 * (Score - 1)
+            WHEN s.Score = 10 THEN (SELECT GlobalMax FROM DateDiffGlobal)
+            ELSE r.MinRecencyValue + (r.MaxRecencyValue - r.MinRecencyValue) / 10.0 * (10 - s.Score)
         END AS LowerLimit,
-        
-        -- For Score 10, UpperLimit is the GlobalMin (most recent date)
+
+        -- For Score 1, UpperLimit is the largest DaysSinceLastOrder (oldest)
         CASE 
-            WHEN Score = 10 THEN (SELECT DATEDIFF(DAY, GlobalMin, GETDATE()) FROM GlobalMinMax)
-            ELSE MinRecencyValue + (MaxRecencyValue - MinRecencyValue) / 10.0 * Score
+            WHEN s.Score = 1 THEN (SELECT GlobalMin FROM DateDiffGlobal)
+            ELSE r.MinRecencyValue + (r.MaxRecencyValue - r.MinRecencyValue) / 10.0 * (10 - (s.Score - 1))
         END AS UpperLimit
     FROM 
-        (SELECT 1 AS Score UNION ALL 
-         SELECT 2 UNION ALL 
-         SELECT 3 UNION ALL 
-         SELECT 4 UNION ALL 
-         SELECT 5 UNION ALL 
-         SELECT 6 UNION ALL 
-         SELECT 7 UNION ALL 
-         SELECT 8 UNION ALL 
+        (SELECT 10 AS Score UNION ALL 
          SELECT 9 UNION ALL 
-         SELECT 10) AS Scores,
-        RecencyQuantileLimits
+         SELECT 8 UNION ALL 
+         SELECT 7 UNION ALL 
+         SELECT 6 UNION ALL 
+         SELECT 5 UNION ALL 
+         SELECT 4 UNION ALL 
+         SELECT 3 UNION ALL 
+         SELECT 2 UNION ALL 
+         SELECT 1) AS s
+    CROSS JOIN RecencyQuantileLimits r
 ),
-
 -- Step 8: Assign unique ID to each score
 FinalScoreAssignments AS (
     SELECT 
@@ -103,4 +106,4 @@ SELECT
 FROM 
     FinalScoreAssignments
 ORDER BY 
-    Score;
+    Score DESC;
